21cb5437f6a205ae0ba90b4ae9bce768e0c8102d
kampfschaft <kampfschaft@gmail.com>
2017-10-04 03:17:50 +0200

    List of states
    + Added combo box for state lists.
    + Named listState.
    ~ TODO: Better UX for listState.
    ~ TODO: listState initially not selected.
    ~ TODO: listState behavior when not selected.

--- PrayerAlarm/Form1.Designer.cs
+++ PrayerAlarm/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.listState = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // notifyIcon1
@@ -40,11 +41,39 @@
             this.notifyIcon1.Visible = true;
             this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
             // 
+            // listState
+            // 
+            this.listState.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.listState.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.listState.FormattingEnabled = true;
+            this.listState.Items.AddRange(new object[] {
+            "Johor",
+            "Kedah",
+            "Kelantan",
+            "Kuala Lumpur",
+            "Melaka",
+            "Negeri Sembilan",
+            "Pahang",
+            "Perak",
+            "Perlis",
+            "Pulau Pinang",
+            "Sabah",
+            "Sarawak",
+            "Selangor",
+            "Terengganu"});
+            this.listState.Location = new System.Drawing.Point(12, 12);
+            this.listState.Name = "listState";
+            this.listState.Size = new System.Drawing.Size(260, 21);
+            this.listState.Sorted = true;
+            this.listState.TabIndex = 0;
+            this.listState.Text = "Select State";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.listState);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Resize += new System.EventHandler(this.Form1_Resize);
@@ -55,6 +84,7 @@
         #endregion
 
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ComboBox listState;
     }
 }
 
